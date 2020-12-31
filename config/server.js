f3ebe9e31c315be45e20e42c6589c7ff19044ec5
Robert Allmond <ballmond@gmail.com>
2020-12-31 15:26:35 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1ba0802cba1026c6ed7b1fe54dec654'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8b1c719-e8fe-4020-acd0-9f6775bf1da5'
+};
